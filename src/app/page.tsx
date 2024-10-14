d50b8cd1b45a071a79f9577021dc68890124a69d
123Mismail <musafirbaltistani786@gmail.com>
2024-10-14 11:53:02 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,121 @@
-"use client";
+"use client"
+import React, { useState } from "react";
+ 
 
-import { useState, useEffect } from "react";
+function page() {
+const [uppercaseAllow,setUpperCaseallowed]=useState(false);
+const [lowercaseAllow,setLowerCaseAllowed]=useState(false);
+const [numberAllow,setNumberAllow]=useState(false);
+const [symbolsAllow,setSymbolaloow]=useState(false);
+const [passwordLength,setPAsswordLength]=useState(0);
+const [password,setPassword]=useState(" ")
 
-export default function Home() {
-  const [time, setTime] = useState(0);
-  const [running, setRunning] = useState(false);
 
-  useEffect(() => {
-    let interval:any;
-    if (running) {
-      interval = setInterval(() => {
-        setTime((pre) => pre + 10);
-      }, 10);
-    } else if(!running) {
-       clearInterval(interval)
+
+   const generatePasswor=()=>{
+    let password="";
+    let str="";
+    if(uppercaseAllow) str +="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if(lowercaseAllow) str +="abcdefghijklmnopqrstuvwxyz";
+    if(numberAllow) str +="0123456789";
+    if(symbolsAllow) str +="!@#$%^&*()_+-={}[]|\:;'<>,.?/";
+
+    for(let i=1 ; i<= passwordLength; i++){
+
+      let char=Math.floor(Math.random()*str.length +1);
+      password+= str.charAt(char);
     }
-    return () => clearInterval(interval);
-  }, [running]);
+
+return password
+
+   }
+
+const onClickGeneratePassword=()=>{
+  const password=generatePasswor();
+  setPassword(password);
+  console.log(password);
+  console.log(passwordLength,"length of password")
+};
+
+const handlePassworCopy=()=>{
+
+     navigator.clipboard.writeText(password);
+     alert("password is copied on clipboard");
+      setPassword("")
+}
+
 
   return (
-    <div className="flex justify-center flex-col items-center h-screen  bg-white">
-      <div className="h-[300px] shadow-blue-900 bg-black max-w-[600px] w-full text-white rounded-lg">
-        <div className="w-full text-2xl font-medium text-white text-center p-3">
-          <h1 className="text-3xl mt-2 font-semibold">Stop Watch</h1>
-        </div>
-        <div className="pl-4 pr-4 mt-6 mx-auto   bg-black text-white text-center text-5xl p-4 font-medium max-w-[220px] w-full rounded-lg ">
-          {" "}
-          <span className="mr-3 ">{Math.floor((time /60000) % 60)}:</span>
-          <span className="mr-2 ml-2">
-            {Math.floor((time /1000) % 60)}:
-          </span>
-          <span>{Math.floor((time / 10) % 100)}</span>
-        </div>
-        <div className="w-full mx-auto text-center pl-4 pr-4  mt-6">
-          <button
-            className="px-4 py-2 rounded-lg  hover:bg-green-400 hover:px-5 hover:py-3 cursor-pointer bg-green-600 text-lg font-medium text-white"
-            onClick={() => setRunning(true)}
-          >
-            start
-          </button>
-          <button
-            className="px-4 py-2 rounded-lg hover:px-5 hover:py-3 cursor-pointer font-medium bg-red-600 text-lg ml-2 mr-2 text-white"
-            onClick={() => setRunning(false)}
-          >
-            stop
-          </button>
-          <button
-            className="px-4 py-2 rounded-lg cursor-pointer hover:px-5 hover:py-3 font-medium bg-blue-600 text-lg text-white"
-            onClick={() => setTime(0)}
-          >
-            reset
-          </button>
-        </div>
+    <div className="mx-auto  lg:mt-[10vh]   items-center lg:max-h-[500px] h-screen   max-w-[500px] w-full bg-gradient-to-b from-blue-600 to-green-600 p-6 text-xl rounded-xl">
+      <div className="pt-2 ">
+        <h1 className="text-center text-3xl font-medium text-white">
+          Password Generator
+        </h1>
+        <p className="pt-2 text-lg ">
+          Create a secure password with just a few clicks.
+        </p>
       </div>
+      <div className="pt-1 pb-1 w-full">
+        <label className="block pb-1 font-medium" htmlFor="#">
+          length{" "}
+        </label>
+        <input className="w-full p-1 rounded-xl outline-none " type="number"
+        onChange={(e:React.ChangeEvent<HTMLElement>)=>setPAsswordLength(e.target.value)}
+        />
+      </div>
+      <div>
+        <h2 className="tex-xl font-medium pt-1 pb-1">Includes:</h2>
+
+        <input type="checkbox" className=" rounded-full"
+        onClick={()=>setNumberAllow(!numberAllow)}
+        />
+        <label className="font-medium text-lg pl-1 ">Numbers</label>
+        <br></br>
+        <input type="checkbox" 
+          onClick={()=>setUpperCaseallowed(!uppercaseAllow)}
+        />
+        <label className="font-medium text-lg pl-1 " htmlFor="#">
+          Uppercase
+        </label>
+        <br></br>
+        <input type="checkbox"
+        onClick={()=>setLowerCaseAllowed(!lowercaseAllow)}
+        />
+        <label className="font-medium text-lg pl-1 " htmlFor="#">
+          Lowercase
+        </label>
+        <br></br>
+        <input type="checkbox" 
+        onClick={()=>setSymbolaloow(!symbolsAllow)}
+        />
+        <label className="font-medium text-lg pl-1 " htmlFor="#">
+          Symbols
+        </label>
+        <br></br>
+      </div>
+      <div className="mx-auto text-center text-lg pt-1 pb-1">
+        <button className="px-10 py-1 w-full rounded-xl bg-black text-white font-medium  hover:opacity-80 "
+        onClick={onClickGeneratePassword}
+        >
+          Generate Password
+        </button>
+      </div>
+      <label className="block pt-1 pb-1 text-lg" htmlFor="#">
+        Generated password{" "}
+      </label>
+      <input
+      readOnly
+        type="text"
+        className={`rounded-xl p-1   outline-none  bg-white  lg:max-w-2/3 w-full `}
+        value={password}
+      />
+      <button className="rounded-xl mt-3 w-full  hover:opacity-80    text-lg font-medium  py-1 bg-black text-white" 
+      onClick={handlePassworCopy}
+      >
+        Copy Password
+      </button>
     </div>
   );
 }
+
+export default page;
